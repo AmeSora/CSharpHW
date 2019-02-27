67631b6ab3925d8b728834ea74f109c70a53792c
AmeSora <tclnomilai@outlook.com>
2019-02-27 18:22:02 +0800

    commit the first homework

---
+++ homework1/hw1_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("请输入第一个数字：");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("请输入第二个数字：");
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine("两个数的积是{0}", a * b);
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("输入有误！");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ homework1/hw1_5/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hw1_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+                textBox3.Text = (a * b).ToString();
+            } catch (Exception)
+            {
+                MessageBox.Show("输入有误！");
+            }
+        }
+    }
+}
