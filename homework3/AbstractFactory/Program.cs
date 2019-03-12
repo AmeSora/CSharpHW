47d5ae1266e6638bc966659b76f08f2de8687a27
AmeSora <tclnomilai@outlook.com>
2019-03-13 00:17:07 +0800

    提交第三次作业

---
+++ homework3/AbstractFactory/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameFactory gameFactory;
+            InterfaceController interfaceController;
+            OperationController operationController;
+
+            Console.WriteLine("AndriodFactory示例：");
+            gameFactory = new AndriodFactory();
+
+            interfaceController = gameFactory.CreateInterfaceController();
+            operationController = gameFactory.CreateOperationController();
+
+            interfaceController.InitInterfaceController();
+            operationController.InitOperationController();
+
+            Console.WriteLine("-----------------------------------");
+
+            Console.WriteLine("SymbianFactory示例：");
+            gameFactory = new SymbianFactory();
+
+            interfaceController = gameFactory.CreateInterfaceController();
+            operationController = gameFactory.CreateOperationController();
+
+            interfaceController.InitInterfaceController();
+            operationController.InitOperationController();
+        }
+    }
+
+    //初始化游戏各组件的工厂接口
+    interface GameFactory
+    {
+        OperationController CreateOperationController();
+        InterfaceController CreateInterfaceController();
+    }
+
+    //操作控制接口
+    interface OperationController
+    {
+        void InitOperationController();
+    }
+
+    //界面控制接口
+    interface InterfaceController
+    {
+        void InitInterfaceController();
+    }
+
+
+    //Symbian系统实现
+    class SymbianOperationController : OperationController
+    {
+        public void InitOperationController()
+        {
+            Console.WriteLine("初始化Symbian系统的Operation Controller");
+        }
+    }
+
+    class SymbianInterfaceController : InterfaceController
+    {
+        public void InitInterfaceController()
+        {
+            Console.WriteLine("初始化Symbian系统的Interface Controller");
+        }
+    }
+
+    class SymbianFactory : GameFactory
+    {
+        public SymbianFactory()
+        {
+            Console.WriteLine("在Symbian系统上启动游戏");
+        }
+        public InterfaceController CreateInterfaceController()
+        {
+            return new SymbianInterfaceController();
+        }
+
+        public OperationController CreateOperationController()
+        {
+            return new SymbianOperationController();
+        }
+
+    }
+
+    //Andriod系统实现
+    class AndriodInterfaceController : InterfaceController
+    {
+        public void InitInterfaceController()
+        {
+            Console.WriteLine("初始化Andriod系统的Interface Controller");
+        }
+    }
+
+    class AndriodOperationController : OperationController
+    {
+        public void InitOperationController()
+        {
+            Console.WriteLine("初始化Andriod系统的Operation Controller");
+        }
+    }
+
+    class AndriodFactory : GameFactory
+    {
+        public AndriodFactory()
+        {
+            Console.WriteLine("在Andriod系统上启动游戏");
+        }
+        public InterfaceController CreateInterfaceController()
+        {
+            return new AndriodInterfaceController();
+        }
+
+        public OperationController CreateOperationController()
+        {
+            return new AndriodOperationController();
+        }
+    }
+
+
+}
---
+++ homework3/Singleton/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventHandler e1;
+            EventHandler e2;
+
+            //单例测试
+            e1 = EventHandler.GetInstance();
+            e2 = EventHandler.GetInstance();
+
+            if (ReferenceEquals(e1,e2))
+                Console.WriteLine("二者是同一对象！");
+            else Console.WriteLine("二者不是同一对象！");
+        }
+    }
+
+
+    //定义单例类
+    class EventHandler
+    {
+        //私有构造方法
+        private EventHandler() { }
+
+        private static class EventHandlerInstance{
+           public readonly static EventHandler instance = new EventHandler();
+        }
+
+        public static EventHandler GetInstance()
+        {
+            return EventHandlerInstance.instance;
+        }
+    }
+}
