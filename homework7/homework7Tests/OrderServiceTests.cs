b81294c8997e027f2942d576d8882923f3f8645f
AmeSora <tclnomilai@outlook.com>
2019-04-17 22:16:08 +0800

    提交第七次作业

---
+++ homework7/homework7Tests/OrderServiceTests.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ordertest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordertest.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        [TestMethod()]
+        public void OrderServiceTest()
+        {
+            OrderService order = new OrderService();
+            Assert.IsNotNull(order);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exception))]
+        public void AddOrderTest()
+        {
+            Customer customer = new Customer(3, "Mike");
+            Order order1 = new Order(1, customer);
+            Order order2 = new Order(1, customer);
+            OrderService orderService = new OrderService();
+
+            orderService.AddOrder(order1);
+            orderService.AddOrder(order2);
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            OrderService order = new OrderService();
+            Customer c1 = new Customer(1, "Mike");
+            Customer c2 = new Customer(2, "Mary");
+            Order order1 = new Order(1, c1);
+            order.AddOrder(order1);
+            Order order2 = new Order(1, c2);
+            order.Update(order2);
+
+            Assert.AreEqual(order.GetById(1).Customer, c2);
+        }
+
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+            OrderService order = new OrderService();
+            Order o1 = new Order(1, new Customer(1, "Mike"));
+            order.AddOrder(o1);
+            Assert.AreEqual(order.GetById(1), o1);
+        }
+
+        [TestMethod()]
+        public void RemoveOrderTest()
+        {
+            OrderService order = new OrderService();
+            Order o = new Order(1, new Customer(1, "mike"));
+            order.AddOrder(o);
+            order.RemoveOrder(1);
+            Assert.IsNull(order.GetById(1));
+        }
+
+        [TestMethod()]
+        public void QueryAllTest()
+        {
+            OrderService order = new OrderService();
+            List<Order> list = order.QueryAll();
+            Assert.AreEqual(list.Count, 0);
+            order.AddOrder(new Order());
+            Assert.AreEqual(order.QueryAll().Count, 1);
+        }
+
+        [TestMethod()]
+        public void QueryByGoodsNameTest()
+        {
+            Goods milk = new Goods(1, "Milk", 69.9f);
+            Customer customer1 = new Customer(1, "Customer1");
+            Order order1 = new Order(1, customer1);
+            order1.AddDetails(new OrderDetail(milk, 10));
+            OrderService order = new OrderService();
+            order.AddOrder(order1);
+            List<Order> re = order.QueryByGoodsName("Milk");
+            Assert.AreEqual(order1, re[0]);
+        }
+
+        [TestMethod()]
+        public void QueryByTotalAmountTest()
+        {
+            Goods milk = new Goods(1, "Milk", 69.9f);
+            Customer customer1 = new Customer(1, "Customer1");
+            Order order1 = new Order(1, customer1);
+            order1.AddDetails(new OrderDetail(milk, 10));
+            OrderService order = new OrderService();
+            order.AddOrder(order1);
+            List<Order> re = order.QueryByTotalAmount(50f);
+            Assert.AreEqual(order1, re[0]);
+        }
+
+        [TestMethod()]
+        public void QueryByCustomerNameTest()
+        {
+            Goods milk = new Goods(1, "Milk", 69.9f);
+            Customer customer1 = new Customer(1, "Customer1");
+            Order order1 = new Order(1, customer1);
+            order1.AddDetails(new OrderDetail(milk, 10));
+            OrderService order = new OrderService();
+            order.AddOrder(order1);
+            List<Order> re = order.QueryByCustomerName("Customer1");
+            Assert.AreEqual(order1, re[0]);
+            List<Order> re1 = order.QueryByCustomerName("Cust");
+            Assert.AreEqual(re1.Count, 0);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortTest()
+        {
+            OrderService order = new OrderService();
+            Order o1 = new Order(1, new Customer());
+            Order o2 = new Order(2, new Customer());
+            Order o3 = new Order(3, new Customer());
+            order.AddOrder(o1);
+            order.AddOrder(o2);
+            order.AddOrder(o3);
+            order.Sort((o_1, o_2) => o_1.Id.CompareTo(o_2.Id));
+            List<Order> re = order.QueryAll();
+            Assert.AreEqual(re[0].Id, 1);
+            Assert.AreEqual(re[1].Id, 2);
+            Assert.AreEqual(re[2].Id, 3);
+            order.Sort(null);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ExportTest()
+        {
+            Goods milk = new Goods(1, "Milk", 69.9f);
+            Customer customer1 = new Customer(1, "Customer1");
+            Order order1 = new Order(1, customer1);
+            order1.AddDetails(new OrderDetail(milk, 10));
+            OrderService order = new OrderService();
+            order.AddOrder(order1);
+
+            order.Export("test.xml");
+            order.Export("text.txt");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ImportTest()
+        {
+            OrderService order = new OrderService();
+            List<Order> re = order.Import("test.xml");
+            Assert.AreEqual(re[0].Id, 1);
+            order.Import("test.txt");
+        }
+    }
+}
---
+++ homework7/hw5_15/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hw5_15
+{
+    public partial class Form1 : Form
+    {
+
+        private Graphics graphics;
+
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = CreateGraphics();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                graphics = splitContainer1.Panel2.CreateGraphics();
+                graphics.TranslateTransform(AutoScrollPosition.X, AutoScrollPosition.Y);
+                double th1 = double.Parse(textBox3.Text) * Math.PI / 180;
+                double th2 = double.Parse(textBox4.Text) * Math.PI / 180;
+                double per1 = double.Parse(textBox1.Text);
+                double per2 = double.Parse(textBox2.Text);
+                this.graphics.Clear(Color.AliceBlue);
+                DrawCayleyTree(10, 300, 410, 120, new double[] { per1, per2 }, -Math.PI / 2, new double[] { th1, th2 });
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("输入有误！");
+            }
+        }
+
+        void DrawCayleyTree(int n, double x0, double y0, double length, double[] per, double th,  double[] th_)
+        {
+            if (n == 0) { return; }
+            double k = 1;
+            if (comboBox3.SelectedIndex != -1)
+            {
+                k = double.Parse(comboBox3.Text);
+            }
+
+            double x1 = x0 + length * Math.Cos(th) * k;
+            double y1 = y0 + length * Math.Sin(th);
+            double x2 = x0 + length * Math.Cos(th) * k;
+            double y2 = y0 + length * Math.Sin(th);
+
+
+            DrawLine(x0, y0, x1, y1);
+    
+            DrawCayleyTree(n - 1, x1, y1, length * per[0], per, th + th_[0], th_);
+            DrawCayleyTree(n - 1, x2, y2, length * per[1], per, th - th_[1], th_);
+
+        }
+
+        void DrawLine(double x0, double y0, double x1, double y1)
+        {
+            Color c = Color.Empty;
+            if (comboBox1.SelectedIndex == -1) c = Color.LightCoral;
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0: c = Color.LightCoral;break;
+                case 1: c = Color.Black;break;
+                case 2: c = Color.Pink;break;
+                case 3: c = Color.LightSkyBlue;break;
+            }
+            float fl = 2f;
+            if (comboBox2.SelectedIndex != -1)
+            {
+                fl = float.Parse(comboBox2.Text);
+            }
+            Pen p = new Pen(c, fl);
+            graphics.DrawLine(p, (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+    }
+}
